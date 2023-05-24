6cc523680a7913cc1bace8385869bc1f2ff4def3
Carina-Firkowski <carina.firkowski@apexrms.com>
2023-05-24 07:48:03 -0400

    Create 0-constants.R

---
+++ Scripts/0-constants.R
@@ -0,0 +1,33 @@
+## a290
+## Carina Rauen Firkowski
+## April 20, 2023
+##
+## This script is used to define constants and load necessary functions in a
+## reproducible way across scripts.
+
+
+
+# Workspace -------------------------------------------------------------------
+
+# Load packages
+library(terra)
+library(sf)
+library(tidyverse)
+library(ggpubr)
+library(rsyncrosim)
+
+
+
+# Directories ------------------------------------------------------------------
+
+# Core directories
+rootDir <- "."
+dataDir          <- file.path(rootDir, "Data")
+intermediatesDir <- file.path(rootDir, "Intermediates")
+outputDir <- file.path(rootDir, "Outputs")
+
+# Composite directories
+tabularDataDir <- file.path(dataDir, "Tabular")
+spatialDataDir <- file.path(dataDir, "Spatial")
+
+
